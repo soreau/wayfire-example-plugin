cdb89a9d11c3a72d18535a3e1d149ddecef8ec96
Scott Moreau <oreaus@gmail.com>
2025-02-15 06:21:04 -0700

    Print mouse cursor position to log

--- src/example-plugin.cpp
+++ src/example-plugin.cpp
@@ -24,19 +24,36 @@
 
 #include <wayfire/view.hpp>
 #include <wayfire/plugin.hpp>
+#include <wayfire/per-output-plugin.hpp>
+#include <wayfire/plugins/common/input-grab.hpp>
 
-class wayfire_example_plugin : public wf::plugin_interface_t
+class wayfire_example_plugin : public wf::per_output_plugin_instance_t, public wf::pointer_interaction_t
 {
+    std::unique_ptr<wf::input_grab_t> input_grab;
+
   public:
     void init() override
     {
         LOGI("Hello World!");
+
+        wf::plugin_activation_data_t grab_interface = {
+            .name = "example-plugin",
+            .capabilities = wf::CAPABILITY_GRAB_INPUT,
+        };
+        input_grab = std::make_unique<wf::input_grab_t>("example-plugin", output, nullptr, this, nullptr);
+        input_grab->grab_input(wf::scene::layer::OVERLAY);
+    }
+
+    void handle_pointer_motion(wf::pointf_t pointer_position, uint32_t time_ms) override
+    {
+        LOGI("Pointer: ", pointer_position.x, ",", pointer_position.y);
     }
 
     void fini() override
     {
         LOGI("Goodbye, Cruel World..");
+        input_grab->ungrab_input();
     }
 };
 
-DECLARE_WAYFIRE_PLUGIN(wayfire_example_plugin);
+DECLARE_WAYFIRE_PLUGIN(wf::per_output_plugin_t<wayfire_example_plugin>);
